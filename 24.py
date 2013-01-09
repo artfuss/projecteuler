a22cde3d588d8e3e507028e4d1d64440a26070b7
artfuss <badonisuneet@gmail.com>
2013-01-09 14:18:20 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -4,9 +4,9 @@ m=[0 for i in xrange(10)]
 p=[i for i in xrange(10)]
 for i in xrange(9,0,-1):
   while n - l > 0:
-		n -= l
-		m[i] += 1
-	l /= i
+	n -= l
+	m[i] += 1
+  l /= i
 import sys
 for i in xrange(9,-1,-1):
 	sys.stdout.write("%d"%p[m[i]])
