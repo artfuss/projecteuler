61611423ec6934ae00a057b2d6f88c19e4601174
unknown <badsu02@GUMKR01-W71.ca.com>
2013-03-08 01:00:53 +0530

    update 20 to oneliner

--- 0020-Factorial digit sum.py
+++ 0020-Factorial digit sum.py
@@ -1,11 +1,4 @@
 #! /usr/bin/env python
-import operator
-def factorial():
-	return reduce(operator.mul,range(1,101))
 
-n = factorial()
-s = 0
-while n:
-	s += n%10
-	n /= 10
-print s
+import operator
+print reduce(operator.add,map(int, str(reduce(operator.mul,range(1,101)))))
