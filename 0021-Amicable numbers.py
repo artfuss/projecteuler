cced416dd4c6fcce9cac93031220dcaa55c82449
artfuss <badonisuneet@gmail.com>
2013-04-04 12:19:44 +0530

    Update 0021-Amicable numbers.py

--- 0021-Amicable numbers.py
+++ 0021-Amicable numbers.py
@@ -1,17 +1,23 @@
 #! /usr/bin/env python
-
+from math import sqrt
 def d(a):
-  sum = 0
-	for i in xrange(1,a):
-		if a%i==0: sum+=i
-	return sum
+    t = a
+    if a==1: return 0
+    sum = 1
+    for i in xrange(2,int(sqrt(a))):
+        j = 1
+        while a%i==0:
+            j += 1
+            a /= i
+        sum *= (((i**j)-1)/(i-1))
+    if a>1: sum *= (a+1)
+    return sum-t
 
 l = []
 sum = 0
-for a in xrange(1,10000):
+for a in xrange(2,10000):
 	b = d(a)
-	if a!=b and b<10000 and a==d(b) and b not in l: 
+	if a!=b and b<10000 and a==d(b) and b not in l:
 		sum = sum+a+b
 		l.append(a)
-
 print sum
