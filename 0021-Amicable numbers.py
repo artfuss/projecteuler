09d5d743073683151c25f28329b300e2dfd0f5c2
artfuss <badoni.suneet@gmail.com>
2014-04-04 20:14:48 +0530

    reformatting and  removed mixed indentation

--- 0021-Amicable numbers.py
+++ 0021-Amicable numbers.py
@@ -1,20 +1,25 @@
 #! /usr/bin/env python
 from math import sqrt
+
+
 def d(a):
-    sum = 1
-    for i in xrange(2,int(sqrt(a))):
+    res = 1
+    for i in xrange(2, int(sqrt(a))):
         j = 1
-        while a%i==0:
+        while a % i == 0:
             j += 1
             a /= i
-        sum *= (i**j-1)/(i-1)
-    if a>1: sum *= a + 1
-    return sum
+        res *= (i ** j - 1) / (i - 1)
+    if a > 1:
+        res *= a + 1
+    return res
 
-sum = 0
-for a in xrange(2,10000):
-	   b = d(a)-a
-	   if a!=b and b<10000 and a==d(b)-b:
-		  sum += a + b
 
-print sum >> 1
+if __name__ == '__main__':
+    res = 0
+    for a in xrange(2, 10000):
+        b = d(a) - a
+        if a != b and b < 10000 and a == d(b) - b:
+            res += a + b
+
+    print res >> 1
